afd949907f722cbe80d563b33c0bfdd1fecefadf
ChristaFox <christa.r.fox@me.com>
2018-06-12 16:28:21 -0700

    feature google plus log-in methods and imports

--- src/components/google-login/google-login.ts
+++ src/components/google-login/google-login.ts
@@ -1,22 +1,68 @@
 import { Component } from '@angular/core';
 
-/**
- * Generated class for the GoogleLoginComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Environment } from './../../environment/environment';
+import { Platform } from 'ionic-angular';
+
+
 @Component({
   selector: 'google-login',
   templateUrl: 'google-login.html'
 })
 export class GoogleLoginComponent {
 
-  text: string;
+  user: Observable<firebase.User>;
 
-  constructor() {
-    console.log('Hello GoogleLoginComponent Component');
-    this.text = 'Hello World';
+  constructor(private afAuth: AngularFireAuth, 
+              private gplus: GooglePlus,
+              private platform: Platform) {
+
+    this.user = this.afAuth.authState;
+
+  }
+
+  /// Our login Methods will go here
+  async nativeGoogleLogin(): Promise<void> {
+    try {
+  
+      const gplusUser = await this.gplus.login({
+        'webClientId': 'environment.webClientId',
+        'offline': true,
+        'scopes': 'profile email'
+      })
+  
+      return await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
+  
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  async webGoogleLogin(): Promise<void> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider);
+  
+    } catch(err) {
+      console.log(err)
+    }
+  
+  }
+
+  googleLogin() {
+    if (this.platform.is('cordova')) {
+      this.nativeGoogleLogin();
+    } else {
+      this.webGoogleLogin();
+    }
+  }
+  
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
