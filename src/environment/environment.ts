6c9ea833477dcd6fee50c8afde331c930c112821
ChristaFox <christa.r.fox@me.com>
2018-06-12 13:17:22 -0700

    deleting info in environment file because it was being tracked.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    firebase: {
-        apiKey: "AIzaSyDhKVIFnhb4VPY2ov3q8ZYFzuQ39atBOjI",
-        authDomain: "cucumber-81d75.firebaseapp.com",
-        databaseURL: "https://cucumber-81d75.firebaseio.com",
-        projectId: "cucumber-81d75",
-        storageBucket: "cucumber-81d75.appspot.com",
-        messagingSenderId: "246481613876"
-    }
-    
+  
 }
