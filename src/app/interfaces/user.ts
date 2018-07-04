66a4f03d051afb898c74ea736533a0c79bc742d4
J.R. Ruggiero <fleury14@comcast.net>
2018-07-04 13:47:56 -0700

    chore(interfaces): decided not to put trash pickup inside user in order to flatten db

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,10 +1,8 @@
-import { ITrashPickup } from "./trash-pickup";
-
 export interface IUser {
     googleId: string;
     displayName: string;
     email: string;
     score: number;
-    trashPickups: ITrashPickup[];
-    
+    trashPickups: string[];
+
 }
