176b76cbc64fc8a3cc0aaa0c02a75c1df56862eb
J.R. Ruggiero <fleury14@comcast.net>
2018-07-15 13:39:21 -0700

    chore(list): tab shows up but doesnt link properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,8 @@ export class MyApp {
     { title: 'Master Detail', component: 'ListMasterPage' },
     { title: 'Menu', component: 'MenuPage' },
     { title: 'Settings', component: 'SettingsPage' },
-    { title: 'Search', component: 'SearchPage' }
+    { title: 'Search', component: 'SearchPage' },
+    { title: 'List', component: 'ListPage'}
   ]
 
   constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
---
+++ src/pages/list-view/list-view.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Settings } from '../../providers';
+import { FormBuilder } from '../../../node_modules/@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+
+@IonicPage()
+@Component({
+    selector: 'list-view',
+    templateUrl: 'list-view.html'
+})
+
+export class ListPage {
+    page: string = 'main';
+    pageTitleKey: string = 'LISTVIEW_TITLE';
+    pageTitle: string;
+
+    constructor(public navCtrl: NavController,
+        public settings: Settings,
+        public formBuilder: FormBuilder,
+        public navParams: NavParams,
+        public translate: TranslateService) {
+      }
+}
