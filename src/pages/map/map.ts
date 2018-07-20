8235d5d439309ae65dbba5dbc5c25e95b33e76bd
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-07-19 17:08:28 -0700

    added marker funcitonality

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,8 +1,9 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams, IonicPage } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-import { GoogleLoginComponent} from '../../components/google-login/google-login';
+import { GoogleLoginService } from '../../components/google-login/google-login';
 import { map } from '../../../node_modules/rxjs/operators';
+import { Http } from '@angular/http';
 
 declare var google;
 @IonicPage()
@@ -12,17 +13,19 @@ declare var google;
 })
 
 export class MapPage {
-  @ViewChild('map') mapElement: ElementRef;
-]map: any;
+  @ViewChild('mapContainer') mapElement: ElementRef;
+  map: any;
   showRecycleView: boolean = false;
   showTrashView:  boolean = true;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, public http: Http) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
     this.loadMap();
+    this.loadRecycleMarkers();
+    this.loadTrashMarkers();
   }
 
   loadMap() {
@@ -49,86 +52,118 @@ export class MapPage {
   };
    
 
-  const recycleLocations = [
-    ['TSG Recycling Disposal', 36.695616, 119.783011, 3],
-    ['Fresno Recycling', 36.725920, -119.763342, 2],
-    ['Barrios Recycling', 36.701790, -119.757840, 1]
-  ];
+  loadRecycleMarkers() {
+    this.http.get('assets/data.recycleMarkers.json')
+    .map((res) => res.json())
+    .subscribe(data => {
+      this.addRecycleMarkersToMap(data);
+    });
+  }
 
-  const trash = [
-    ['Mid Valley Disposal', 36.702229, -119.788517, 1],
-    ['Orange Avenue Disposal', 36.685982, -119.755236, 2]
-  ];
+  loadTrashMarkers() {
+    this.http.get('assets/data.trashMarkers.json')
+    .map((res) => res.json())
+    .subscribe(data => {
+      this.addTrashMarkersToMap(data);
+    });
+  }
 
-  const marker, i;
-  
-    showRecycleMarkers() {
-      for (i = 0; i < this.recycleLocations.length; i++ ) {
-        marker = new google.maps.Marker({
-          positon: new google.maps.LatLng(recycleLocations[i][1], [i][2]), )
-          map: map
-
-      });
-       
-      google.maps.event.addListener(marker, 'click' (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    };
-
-    showTrashMarkers() {
-      for (i = 0; i < this.trashLocations.length; i++ ) {
-        marker = new google.maps.Marker({
-          positon: new google.maps.LatLng(trashLocations[i][1], [i][2]), )
-          map: map
-
-      });
-       
-      google.maps.event.addListener(marker, 'click' (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    };
-
-
-    toggleRecycle() {
-      this.showRecycleView = !this.showRecycleView;
-      this.showRecycleMarkers();
+  addRecycleMarkersToMap(markers) {
+    for(let marker of markers) {
+      const position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      const recycleMarker = new google.maps.Marker({position: position, title: marker.name});
+      recycleMarker.setMap(this.map);
     }
+  }
+
+  addTrashMarkersToMap(markers) {
+    for(let marker of markers) {
+      const position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      const trashMarker = new google.maps.Marker({position: position, title: marker.name});
+      trashMarker.setMap(this.map);
+    }
+  }
+
+  toggleRecycle(recycleMarker) {
+    this.showRecycleView = !this.showRecycleView;
+   }
     
-    toggleTrash() {
-      this.showTrashView = !this.showTrashView;
-      this.showTrashMarkers();
-    }
+  toggleTrash(trashMarker) {
+    this.showTrashView = !this.showTrashView;
+  
+  }
+}
+
+//   const recycleLocations = [
+//     ['TSG Recycling Disposal', 36.695616, 119.783011, 3],
+//     ['Fresno Recycling', 36.725920, -119.763342, 2],
+//     ['Barrios Recycling', 36.701790, -119.757840, 1]
+//   ];
+
+//   const trash = [
+//     ['Mid Valley Disposal', 36.702229, -119.788517, 1],
+//     ['Orange Avenue Disposal', 36.685982, -119.755236, 2]
+//   ];
+
+//   const marker, i;
+  
+//     showRecycleMarkers() {
+//       for (i = 0; i < this.recycleLocations.length; i++ ) {
+//         this.marker = new google.maps.Marker({
+//           positon: new google.maps.LatLng(this.recycleLocations[i][1], [i][2]), )
+//           map: map
+
+//       });
+       
+//       google.maps.event.addListener(marker, 'click' (function(marker, i) {
+//         return function() {
+//           infowindow.setContent(locations[i][0]);
+//           infowindow.open(map, marker);
+//         }
+//       })(marker, i));
+//     };
+
+//     showTrashMarkers() {
+//       for (i = 0; i < this.trashLocations.length; i++ ) {
+//         marker = new google.maps.Marker({
+//           positon: new google.maps.LatLng(trashLocations[i][1], [i][2]), )
+//           map: map
+
+//       });
+       
+//       google.maps.event.addListener(marker, 'click' (function(marker, i) {
+//         return function(){
+//           infowindow.setContent(locations[i][0]);
+//           infowindow.open(map, marker);
+//         }
+//       })(marker, i));
+//     };
+
+
 
   
 
 
-      // addMarker() {
-    //   let marker = new google.maps.Marker({
-    //     map: this.map,
-    //     animation: google.maps.Animation.DROP,
-    //     position: this.map.getCenter()
-    //   });
+//       // addMarker() {
+//     //   let marker = new google.maps.Marker({
+//     //     map: this.map,
+//     //     animation: google.maps.Animation.DROP,
+//     //     position: this.map.getCenter()
+//     //   });
 
-    //   let content = '<h4>Information!</h4>';
+//     //   let content = '<h4>Information!</h4>';
       
-    //   this.addInfoWindow(marker, content);
-    // }
+//     //   this.addInfoWindow(marker, content);
+//     // }
 
-    // addInfoWindow(marker, content) {
-    //   let infoWindow = new google.maps.infoWindow({
-    //     content: content
-    //   });
+//     // addInfoWindow(marker, content) {
+//     //   let infoWindow = new google.maps.infoWindow({
+//     //     content: content
+//     //   });
 
-    //   google.maps.event.addListener(marker , 'click', () => {
-    //     infoWindow.open(this.map, marker);
+//     //   google.maps.event.addListener(marker , 'click', () => {
+//     //     infoWindow.open(this.map, marker);
 
-    //   });
+//     //   });
 
-    // }
-}
+//     
