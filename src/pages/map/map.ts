390c03e167694f79659b49c57a206f45974eba64
brandynm34 <brandynm34@yahoo.com>
2018-07-22 20:27:19 -0700

    deleted css files and added features to map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams, IonicPage } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation';
 import { HttpClient } from '@angular/common/http';
+import { ToastController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
-
-declare var google;
 @IonicPage()
 @Component({
   selector: 'page-map',
@@ -13,105 +12,110 @@ declare var google;
 })
 
 export class MapPage {
+  // google;
  
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-//   showRecycleView: boolean = false;
-//   showTrashView:  boolean = true;
+  showRecycleView: boolean = true;
+  showTrashView:  boolean = true;
+  public recycleList: any[] = []; 
+  public trashList: any[] = [];
 
  
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, private http: HttpClient) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, private http: HttpClient, private toastCtrl: ToastController ) { }
   
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+  ionViewWillEnter() {
     this.loadMap();
-    // this.loadRecycleMarkers();
-    // this.loadTrashMarkers();
   }
 
   loadMap() {
-    this.geolocation.getCurrentPosition().then((position) => {
+    this.geolocation.getCurrentPosition().then((position:any) => {
+      // console.log(position)
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
       let mapOptions = {
         center: latLng,
-        zoom: 15, 
+        zoom: 10, 
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.loadRecycleMarkers();
+      this.loadTrashMarkers();
+    }, err => {
+      const toast = this.toastCtrl.create({
+        message: 'Please enable your location services for this app to use this feature',
+        duration: 3000
+      });
+      toast.present();
     });
   }
-};
 
-//   loadRecycleMarkers() {
-//     this.http.get('../../assets/data/recycleMarkers.json')
-//       .subscribe(data => {
-//       this.addRecycleMarkersToMap(data);
-//     });
-//   }
-  
-//   loadTrashMarkers() {
-//     this.http.get('../../assets/data/trashMarker.json')
-//     .subscribe(data => {
-//       this.addTrashMarkersToMap(data);
-//     });
-//   }
+  loadRecycleMarkers() {
+    this.http.get('../../assets/data/recycleMarkers.json')
+      .subscribe(data => {
+        this.addRecycleMarkersToMap(data);
+    });
+  }
 
-//   addRecycleMarkersToMap(markers) {
-//     for(let marker of markers) {
-//       const position = new google.maps.LatLng(marker.latitude, marker.longitude);
-//       const recycleMarker = new google.maps.Marker({position: position, title: marker.name});
-//       recycleMarker.setMap(this.map);
-//     }
-//   }
+  addRecycleMarkersToMap(markers) {
+    for(let marker of markers) {
+      const infowindow = new google.maps.InfoWindow({
+        content: marker.name
+      });
+      const position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      const recycleMarker = new google.maps.Marker({position: position, 
+                                                    title: marker.name, 
+                                                    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                                                    type: 'recyle'});
+      recycleMarker.setMap(this.map);
+      google.maps.event.addListener(recycleMarker, 'click', () => {
+        infowindow.open(this.map, recycleMarker);
+      }) 
+      this.recycleList.push(recycleMarker)
+    }
+  }
 
-//   addTrashMarkersToMap(markers) {
-//     for(let marker of markers) {
-//       const position = new google.maps.LatLng(marker.latitude, marker.longitude);
-//       const trashMarker = new google.maps.Marker({position: position, title: marker.name});
-//       trashMarker.setMap(this.map);
-//     }
-//   }
+  loadTrashMarkers() {
+    this.http.get('../../assets/data/trashMarker.json')
+    .subscribe(data => {
+      this.addTrashMarkersToMap(data);
+    });
+  }
 
-//   toggleRecycle(recycleMarker) {
-//     this.showRecycleView = !this.showRecycleView;
-//     }
-    
-//   toggleTrash(trashMarker) {
-//     this.showTrashView = !this.showTrashView;
-// }
+    addTrashMarkersToMap(markers) {
+    for(let marker of markers) {
+      const infowindow = new google.maps.InfoWindow({
+        content: marker.name
+      });
+      const position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      const trashMarker = new google.maps.Marker({position: position, 
+                                                  title: marker.name,
+                                                  type: 'trash'});
+      trashMarker.setMap(this.map);
+      google.maps.event.addListener(trashMarker, 'click', () => {
+        infowindow.open(this.map, trashMarker);
+      })
+      this.trashList.push(trashMarker);
+    }
+  }
 
-
-
-
-
-//   addMarker() {
-//     let marker = new google.maps.Marker({
-//       map: this.map,
-//       animation: google.maps.Animation.DROP,
-//       position: this.map.getCenter()
-//     });
-
-//     let content = '<h4>Information!</h4>';
-    
-//     this.addInfoWindow(marker, content);
-//   }
-
-//   addInfoWindow(marker, content) {
-//     let infoWindow = new google.maps.infoWindow({
-//       content: content
-//     });
-  
-//     google.maps.event.addListener(marker , 'click', () => {
-//       infoWindow.open(this.map, marker);
-//     });
-//   }
-
-
-
-
-
+  toggleTrash(){
+    for(let recycle of this.recycleList){
+      recycle.setMap(null)
+    }
+    for(let trash of this.trashList){
+      trash.setMap(this.map)
+    }
+  }
+        
+  toggleRecycle() {
+    for(let trash of this.trashList){
+      trash.setMap(null)
+    }
+    for(let recycle of this.recycleList){
+      recycle.setMap(this.map)
+    }
+  }
+}
  
-
-
